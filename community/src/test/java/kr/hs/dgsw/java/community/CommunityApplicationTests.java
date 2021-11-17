1deb8eebbbcf1347974f0121f010d556c1071da7
tmddusqls <tmddusqls@naver.com>
2021-11-17 14:45:24 +0900

    init

---
+++ community/src/test/java/kr/hs/dgsw/java/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.java.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
